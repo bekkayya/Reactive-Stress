aa2f9f49e14aa7ace6e375db0d8a1fcaafe734a0
bekkayya <153224752+bekkayya@users.noreply.github.com>
2026-06-29 18:12:35 +0000

    Update UtilityHelperClass to cache momentum extraction config values

--- src/main/java/com/Peterhun/create_reactive_stress/UtilityHelperClass.java
+++ src/main/java/com/Peterhun/create_reactive_stress/UtilityHelperClass.java
@@ -4,7 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.Unique;
 
-import static com.Peterhun.create_reactive_stress.Config.MULTIPLIERS;
+import static com.Peterhun.create_reactive_stress.Config.*;
 
 public abstract class UtilityHelperClass{
     @Unique
@@ -25,6 +25,16 @@ public abstract class UtilityHelperClass{
     @Unique
     public static double createReactiveStress$valueCrushingWheel = 1f;
 
+    // Momentum extraction config caches
+    @Unique
+    public static boolean createReactiveStress$enableMomentumExtraction = true;
+    @Unique
+    public static double createReactiveStress$momentumDrainRate = 0.02;
+    @Unique
+    public static double createReactiveStress$speedThreshold = 0.1;
+    @Unique
+    public static double createReactiveStress$stressDeficitThreshold = 0.1;
+
     public static final Logger LOGGER = LogManager.getLogger("Create_Reactive_Stress");
 
 
@@ -40,6 +50,13 @@ public abstract class UtilityHelperClass{
         createReactiveStress$valueMillstone = MULTIPLIERS.get("MillStone").get();
         createReactiveStress$valueSaw = MULTIPLIERS.get("Saw").get();
         createReactiveStress$valueCrushingWheel = MULTIPLIERS.get("CrushingWheel").get();
+        
+        // Load momentum extraction config values
+        createReactiveStress$enableMomentumExtraction = ENABLE_MOMENTUM_EXTRACTION.get();
+        createReactiveStress$momentumDrainRate = MOMENTUM_DRAIN_RATE.get();
+        createReactiveStress$speedThreshold = SPEED_THRESHOLD.get();
+        createReactiveStress$stressDeficitThreshold = STRESS_DEFICIT_THRESHOLD.get();
+        
         createReactiveStress$cachedValue = true;
         LOGGER.info("Config for Create_Reactive_Stress has been loaded");
     }
